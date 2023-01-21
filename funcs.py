f276bf38465ebd36628c8acc45cde965789ba5f3
karapuchlo <mc_gra_dy@mail.ru>
2023-01-21 19:35:33 +0300

    добавил разность

--- funcs.py
+++ funcs.py
@@ -1,3 +1,7 @@
-def print_hi(n1, n2):
+def Summ(n1, n2):
  Summ = n1 + n2
-    print(f'{Summ}')  # Press Ctrl+F8 to toggle the breakpoint.
+ print(f'{Summ}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+def Deff(n1, n2):
+ Deff = n1 - n2
+ print(f'{Deff}')  # Press Ctrl+F8 to toggle the breakpoint.
